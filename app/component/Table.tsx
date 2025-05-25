8fc6e00d49d16c8505b5517dcd3c50a7625aa8a3
mamun2519 <juborajislam46@gmail.com>
2025-05-25 15:27:56 +0600

    feat: connect to api all account

--- app/component/Table.tsx
+++ app/component/Table.tsx
@@ -51,7 +51,7 @@ export default async function TablePage({ accounts }) {
         <div className="bg-white rounded-lg shadow-md border border-gray-200">
           <div className="p-6 pb-4">
             <h2 className="text-xl font-semibold text-gray-900">
-              Profiles Table
+              Account Table
             </h2>
             <p className="text-sm text-gray-600 mt-1">
               {accounts.length === 0
